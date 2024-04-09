85de0d1038079c147212435f784a8e64ef0728b7
Rohit Mandal <60247178+Ro706@users.noreply.github.com>
2024-04-09 22:23:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ API_KEY = "YOUR_API_KEY"
 palm.configure(api_key = API_KEY)
 prompt = str(input("Enter your prompt:"))
 response=palm.chat(messages=prompt,temperature=0.2,context="Speak like a AI assistance")
+
+# for message in response.messages:
+    # print(message['author'], message['content'])
 for message in response.messages:
             for i in message['content'].split('\n'):
                     if count != 0:    # this will Separate the prompt form the message
